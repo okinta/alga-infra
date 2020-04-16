c9b09ccd002b2ceb6862321b7e2cfbb4f6f10255
Robert Roose <robert@shivercube.com>
2020-04-16 15:32:56 -0300

    Added script to setup iqfeed on a windows server

---
+++ setup-windows-iqfeed.ps1
@@ -0,0 +1,34 @@
+#
+# Installs and configures IQFeed client to run and be accessible within the Vultr private network
+#
+
+param (
+    [Parameter(Mandatory=$true)][string]$ip
+)
+
+$ErrorActionPreference = "Stop"
+
+$IQFeedVersion = "6_1_0_20"
+
+# Configure Vultr private networking
+netsh interface ip set address name="Ethernet 2" static $ip 255.255.0.0 0.0.0.0 1
+
+# Download installers
+Invoke-WebRequest -Uri "http://www.iqfeed.net/iqfeed_client_$IQFeedVersion.exe" -OutFile iqfeed.exe
+
+# Install IQFeed
+Start-Process -Wait -FilePath iqfeed.exe -ArgumentList "/S" -PassThru
+
+# Remove installers
+Remove-Item vc_redist.x86.exe -Force
+Remove-Item iqfeed.exe -Force
+
+# Forward ports so they ca be accessed from the outside
+netsh interface portproxy add v4tov4 listenport=5009 listenaddress=$ip connectaddress=127.0.0.1
+netsh interface portproxy add v4tov4 listenport=9100 listenaddress=$ip connectaddress=127.0.0.1
+netsh interface portproxy add v4tov4 listenport=9200 listenaddress=$ip connectaddress=127.0.0.1
+netsh interface portproxy add v4tov4 listenport=9300 listenaddress=$ip connectaddress=127.0.0.1
+netsh interface portproxy add v4tov4 listenport=9400 listenaddress=$ip connectaddress=127.0.0.1
+
+# Allow IQFeed client access through firewall
+New-NetFirewallRule -Name iqfeed -DisplayName "IQFeed Port Forwarding" -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalAddress "$ip" -LocalPort 5009,9100,9200,9300,9400
