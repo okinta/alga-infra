e22a09092d5b2b6c4450b3ac07d7b2f9c7b48ff8
Robert Roose <robert@shivercube.com>
2020-04-16 20:04:12 -0300

    Added script to run IQFeed client continuously

---
+++ run-iqfeed.ps1
@@ -0,0 +1,15 @@
+#
+# Runs IQFeed client continuously
+#
+
+param (
+    [Parameter(Mandatory=$true)][string]$product,
+    [Parameter(Mandatory=$true)][string]$version,
+    [Parameter(Mandatory=$true)][string]$login,
+    [Parameter(Mandatory=$true)][string]$password,
+)
+
+While ($true) {
+    Start-Process -Wait -FilePath 'C:\Program Files (x86)\DTN\IQFeed\iqconnect.exe' -ArgumentList "-product $product -version $version -login $login -password $password -autoconnect -savelogininfo"
+    Start-Sleep -Seconds 1
+}
