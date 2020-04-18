9de236feca77b77ce92c049e4c529daf35df890f
Robert Roose <robert@shivercube.com>
2020-04-18 18:14:30 -0300

    Updated the way we create a flatcar server so we can pass a custom ignition

--- coreos/install.bash
+++ coreos/install.bash
@@ -4,24 +4,33 @@
 # Installs coreos on a machine
 #
 
-wget https://raw.github.com/coreos/init/master/bin/coreos-install
-chmod +x coreos-install
-./coreos-install -d /dev/vda -C stable
+apt install -y jq
 
-# Set up Vultr CLI so we can eject the ISO and boot into coreos
+# Set up Vultr CLI
 VULTR_CLI_VERSION="0.3.0"
 wget "https://github.com/vultr/vultr-cli/releases/download/v0.3.0/vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
 tar -xzf "vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
 mv ./vultr-cli /usr/local/bin/
 rm -f "vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
+export VULTR_API_KEY=$(cat /root/.bashrc | grep "export VULTR_API_KEY" | awk '{print $2}' | awk -F "=" '{print $2}')
+
+# Who are we?
+id="$(curl -s http://169.254.169.254/v1.json | jq '.instanceid' | tr -d '"')"
+tag=$(vultr-cli serer info $id | grep Tag | awk '{print $2}')
+
+# Install flatcar if that's what the server is destined for
+if [ $tag = "flatcar" ]; then
+    wget https://raw.githubusercontent.com/flatcar-linux/init/flatcar-master/bin/flatcar-install
+    chmod +x flatcar-install
+    ./flatcar-install -d /dev/vda -C stable
+
+# Otherwise install coreos
+else
+    wget https://raw.github.com/coreos/init/master/bin/coreos-install
+    chmod +x coreos-install
+    ./coreos-install -d /dev/vda -C stable
+fi
 
 # Tell Vultr to eject the ISO. This will cause the server to reboot
-export VULTR_API_KEY=$(cat /root/.bashrc | grep "export VULTR_API_KEY" | awk '{print $2}' | awk -F "=" '{print $2}')
-external_ip=$(ifconfig ens3 | grep "inet " | awk '{print $2}')
-id=$(vultr-cli server list | grep "$external_ip" | awk '{print $1}')
-if [ -z "$id" ]; then
-    echo "Can't find server"
-    exit 1
-fi
 echo "Rebooting"
 vultr-cli server iso detach $id
--- setup-flatcar.bash
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/okinta/vultr-scripts/master/setup-flatcar.script.bash)" "" "[VULTR_API_KEY]"
