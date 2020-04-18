45be52ac9fcd46a9d7c2b9d37bce93dfc806e743
Robert Roose <robert@shivercube.com>
2020-04-18 15:29:33 -0300

    Creating default coreos installation

--- coreos/install.bash
+++ coreos/install.bash
@@ -4,4 +4,23 @@
 # Installs coreos on a machine
 #
 
-echo "We're running"
+wget https://raw.github.com/coreos/init/master/bin/coreos-install
+chmod +x coreos-install
+./coreos-install -d /dev/vda -C stable
+
+# Set up Vultr CLI so we can eject the ISO and boot into coreos
+VULTR_CLI_VERSION="0.3.0"
+wget "https://github.com/vultr/vultr-cli/releases/download/v0.3.0/vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
+tar -xzf "vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
+mv ./vultr-cli /usr/local/bin/
+rm -f "vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
+
+# Tell Vultr to eject the ISO. This will cause the server to reboot
+external_ip=$(ifconfig ens3 | grep "inet " | awk '{print $2}')
+id=$(vultr-cli server list | grep "$external_ip" | awk '{print $1}')
+if [ -z "$id" ]; then
+    echo "Can't find server"
+    exit 1
+fi
+echo "Rebooting"
+vultr-cli server iso detach $id
