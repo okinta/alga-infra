1552d6b54d8d52fc2ce016f2e62329b450a698ea
Robert Roose <robert@shivercube.com>
2020-04-18 17:53:18 -0300

    Using Vultr's metadata API I just discovered

--- setup-ubuntu.bash
+++ setup-ubuntu.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/okinta/vultr-scripts/master/setup-ubuntu.script.bash)" "" "[VULTR_API_KEY]"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/okinta/vultr-scripts/master/setup-ubuntu.script.bash)"
--- setup-ubuntu.script.bash
+++ setup-ubuntu.script.bash
@@ -6,30 +6,9 @@ set -e
 # Configures the server's private IP and sets zsh as the default shell
 #
 
-if [ -z "$1" ]; then
-    echo "Vultr API key must be provided"
-    exit 1
-fi
-
-# Set up Vultr CLI so we can find our internal IP
-export VULTR_API_KEY="$1"
-VULTR_CLI_VERSION="0.3.0"
-wget "https://github.com/vultr/vultr-cli/releases/download/v0.3.0/vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
-tar -xzf "vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
-mv ./vultr-cli /usr/local/bin/
-rm -f "vultr-cli_${VULTR_CLI_VERSION}_linux_64-bit.tar.gz"
-
-# Now find this machine's private IP
-external_ip=$(ifconfig ens3 | grep "inet " | awk '{print $2}')
-id=$(vultr-cli server list | grep "$external_ip" | awk '{print $1}')
-if [ -z "$id" ]; then
-    echo "Can't find server"
-    exit 1
-fi
-
-private_ip=$(vultr-cli server info $id | grep "Internal IP" | awk '{print $3}')
-
 # Configure this machine's private network
+apt install -y jq
+private_ip="$(curl -s http://169.254.169.254/v1.json | jq '.interfaces[1].ipv4.address' | tr -d '"')"
 echo "network:
   version: 2
   renderer: networkd
@@ -54,5 +33,5 @@ echo "Finished configuring zsh"
 apt upgrade -y
 apt autoremove -y
 apt-get autoclean -y
-echo "Done. Rebooting."
+echo "Done updating. Rebooting."
 reboot now
