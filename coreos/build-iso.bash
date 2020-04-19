c61d270ff1a06b66bca0193e253a3da6bfc27b3f
Robert Roose <robert@shivercube.com>
2020-04-18 21:39:38 -0300

    Fixed errors

--- coreos/build-iso.bash
+++ coreos/build-iso.bash
@@ -7,10 +7,10 @@ PLAN="1024 MB RAM,25 GB SSD,1.00 TB BW"
 REGION="New Jersey"
 SCRIPT="create-live-iso"
 
-OS_ID=$(vultr-cli os | grep "$APP" | awk '{print $1}')
+OS_ID=$(vultr-cli os | grep "$OS" | awk '{print $1}')
 REGION_ID=$(vultr-cli regions list | grep "$REGION" | awk '{print $1}')
 SCRIPT_ID=$(vultr-cli script list | grep "$SCRIPT" | awk '{print $1}')
 PLAN_ID=$(vultr-cli plans list | grep "$PLAN" | awk '{print $1}')
 
 set -x
-vultr-cli server create --region $REGION_ID --od $OS_ID --plan $PLAN_ID --script-id $SCRIPT_ID
+vultr-cli server create --region $REGION_ID --os $OS_ID --plan $PLAN_ID --script-id $SCRIPT_ID
