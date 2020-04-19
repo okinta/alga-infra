acd0b290988a4eea23b2027a7bf3e48309bf8369
Robert Roose <robert@shivercube.com>
2020-04-19 14:09:11 -0300

    Using Windows Server 2019 for iqfeed

--- create-iqfeed-server.bash
+++ create-iqfeed-server.bash
@@ -2,16 +2,14 @@
 
 set -e
 
+ISO="server2019.iso"
 LABEL="iqfeed"
-OS="Windows 2016 x64"
-PLAN="8192 MB RAM,160 GB SSD,4.00 TB BW"
+PLAN="1024 MB RAM,25 GB SSD,1.00 TB BW"
 REGION="Chicago"
-SCRIPT="setup-windows-iqfeed"
 
-OS_ID=$(vultr-cli os | grep "$OS" | awk '{print $1}')
+ISO_ID=$(vultr-cli iso private | grep "$ISO" | awk '{print $1}')
 REGION_ID=$(vultr-cli regions list | grep "$REGION" | awk '{print $1}')
-SCRIPT_ID=$(vultr-cli script list | grep "$SCRIPT" | awk '{print $1}')
 PLAN_ID=$(vultr-cli plans list | grep "$PLAN" | awk '{print $1}')
 
 set -x
-vultr-cli server create --region $REGION_ID --os $OS_ID --plan $PLAN_ID --private-network true --script-id $SCRIPT_ID --label $LABEL
+vultr-cli server create --region $REGION_ID --iso $ISO_ID --plan $PLAN_ID --private-network true --label "$LABEL"
