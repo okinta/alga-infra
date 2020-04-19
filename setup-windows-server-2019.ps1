014c524e5a1faade1bcfd25152fc69a813820357
Robert Roose <robert@shivercube.com>
2020-04-19 13:41:24 -0300

    Setting up private networking for Windows servers

--- setup-windows-server-2019.ps1
+++ setup-windows-server-2019.ps1
@@ -4,3 +4,10 @@
 
 $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+# Set up Vultr private networking
+$Metadata = Invoke-WebRequest -Uri "http://169.254.169.254/v1.json" | ConvertTo-Json
+$ip = $Metadata.interfaces[1].ipv4.address
+if ([string]::IsNullOrEmpty($ip)) {
+    netsh interface ip set address name="Ethernet 2" static $ip 255.255.0.0 0.0.0.0 1
+}
