c8686a8e5e816452252119cdf354997935453510
Robert Roose <robert@shivercube.com>
2020-04-25 21:36:29 -0300

    Added script to set up Vault

---
+++ setup-vault.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+ip=$(./server stack-vault | tail -1)
+echo "Started server; waiting for it to come online"
+./wait.sh "$ip"
+echo "Vault is online; loading data"
+ssh -oStrictHostKeyChecking=no "$ip" 'bash -s' < ./vault-commands.sh
+echo "Ready"
