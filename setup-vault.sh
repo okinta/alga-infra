e5534f7fcc4236fcb8d76a9ce2a9f61a8f100ac7
Robert Roose <robert@shivercube.com>
2020-04-25 22:19:59 -0300

    Login first to accept key, then run commands

--- setup-vault.sh
+++ setup-vault.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-ip=$(./server stack-vault | tail -1)
+output=$(./server stack-vault)
+echo "$output"
+ip=$(tail -1 <<< "$output")
 echo "Started server; waiting for it to come online"
 ./wait.sh "$ip"
 echo "Vault is online; loading data"
-ssh -oStrictHostKeyChecking=no "$ip" 'bash -s' < ./vault-commands.sh
+ssh -oStrictHostKeyChecking=no "$ip" exit
+ssh "$ip" 'bash -s' < ./vault-commands.sh
 echo
 echo "Ready"
