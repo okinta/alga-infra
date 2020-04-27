7fd9bc72d721285845d98de1c582280f294972e0
Robert Roose <robert@shivercube.com>
2020-04-27 15:00:57 -0300

    Updated script to use --wait flag

--- setup-vault.sh
+++ setup-vault.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-output=$(./server stack-vault)
-echo "$output"
-ip=$(tail -1 <<< "$output")
-echo "Started server; waiting for it to come online"
-./wait.sh "$ip"
-echo "Vault is online; loading data"
+echo "Starting server; waiting for it to come online"
+output=$(./server stack-vault --wait 2>&1 | tee /dev/tty)
+ip=$(grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" <<< "$output")
 ssh -oStrictHostKeyChecking=no "$ip" exit
 sleep 5
 ssh "$ip" 'bash -s' < ./vault-commands.sh
