a3c8022217efadec56d54f2c3df126d18345809a
Robert Roose <robert@shivercube.com>
2020-04-29 11:51:41 -0300

    Keep sending setup instructions to Vault until we get confirmation

--- setup-vault.sh
+++ setup-vault.sh
@@ -3,8 +3,14 @@
 echo "Starting server; waiting for it to come online"
 output=$(./server stack-vault --wait 2>&1 | tee /dev/tty)
 ip=$(grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" <<< "$output")
+
+# Accept the machine's key
 ssh -oStrictHostKeyChecking=no "$ip" exit
-sleep 5
-ssh "$ip" 'bash -s' < ./vault-commands.sh
-echo
+
+# Run the script (keep running until we get confirmation)
+output=$(ssh "$ip" 'bash -s' < ./vault-commands.sh)
+until [[ "$output" == *"key was successfully"* ]]; do
+    output=$(ssh "$ip" 'bash -s' < ./vault-commands.sh)
+done
+echo "$output"
 echo "Ready"
