a3640ff492b29b9c515c2d8592fb1844545b9f47
Robert Roose <robert@shivercube.com>
2020-05-04 12:39:33 -0300

    Updated Vultr script with new args

--- vultr-scripts/create-live-iso.bash
+++ vultr-scripts/create-live-iso.bash
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/okinta/vultr-scripts/master/coreos/create-live-iso.script.bash)" "" "[VULTR_API_KEY]" "[LOGDNA_INGESTION_KEY]"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/okinta/vultr-scripts/master/coreos/create-live-iso.script.bash)" "" \
+    "[VULTR_API_KEY]" \
+    "[CLOUDFLARE_EMAIL]" \
+    "[CLOUDFLARE_API_KEY]" \
+    "[CLOUDFLARE_ZONENAME]" \
+    "[CLOUDFLARE_RECORDNAME]" \
+    "--logdna-ingestion-key" "[LOGDNA_INGESTION_KEY]"
